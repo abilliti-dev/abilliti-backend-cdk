a0dc6b53b67e01f7db7dbc3e78d428265a9237a1
Taylor Finelli <taylorfinelli25@gmail.com>
2024-09-20 18:58:57 -0700

    invoice s3 bucket into CDK code

--- lib/abilliti-backend-stack.ts
+++ lib/abilliti-backend-stack.ts
@@ -4,19 +4,21 @@ import { Construct } from "constructs";
 import { AbillitiCognitoUserPool } from "./constructs/cognito-user-pool";
 import { Table } from "aws-cdk-lib/aws-dynamodb";
 import { InvoiceDDB } from "./constructs/invoice-ddb";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { InvoiceS3Bucket } from "./constructs/invoice-s3-bucket";
 
 export class AbillitiBackendStack extends Stack {
-	readonly userPool: UserPool;
-	readonly invoiceTable: Table;
+  readonly userPool: UserPool;
+  readonly invoiceTable: Table;
+  readonly invoiceS3Bucket: Bucket;
 
-	constructor(scope: Construct, id: string, props?: StackProps) {
-		super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-		this.userPool = new AbillitiCognitoUserPool(
-			this,
-			"AbillitiCognitoUserPool"
-		).userPool;
+    this.userPool = new AbillitiCognitoUserPool(this, "AbillitiCognitoUserPool").userPool;
 
-		this.invoiceTable = new InvoiceDDB(this, "AbillitiInvoiceTable").table;
-	}
+    this.invoiceTable = new InvoiceDDB(this, "AbillitiInvoiceTable").table;
+
+    this.invoiceS3Bucket = new InvoiceS3Bucket(this, "AbillitiInvoiceS3Bucket").bucket;
+  }
 }
