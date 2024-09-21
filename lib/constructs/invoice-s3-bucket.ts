0e7098bd0a5bf8415de1f4437bf88ff71cd9eafa
Taylor Finelli <85365584+taylorfinelli@users.noreply.github.com>
2024-09-20 19:01:15 -0700

    invoice s3 bucket into CDK code (#10)

---
+++ lib/constructs/invoice-s3-bucket.ts
@@ -0,0 +1,24 @@
+import { RemovalPolicy } from "aws-cdk-lib";
+import { BlockPublicAccess, Bucket, BucketEncryption } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+
+export class InvoiceS3Bucket extends Construct {
+  readonly bucket: Bucket;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.bucket = this.createBucket();
+  }
+
+  private createBucket(): Bucket {
+    return new Bucket(this, "InvoiceBucket", {
+      bucketName: "invoice-bucket",
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      bucketKeyEnabled: true,
+      encryption: BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+  }
+}
