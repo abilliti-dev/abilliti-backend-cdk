eacb5df6ac75dd24cb5d387da46eb2ae6f068787
Emanuel Pecson <emanpecson@gmail.com>
2024-09-20 18:39:20 -0700

    dynamodb invoice table setup

--- lib/abilliti-backend-stack.ts
+++ lib/abilliti-backend-stack.ts
@@ -2,16 +2,21 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import { UserPool } from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
 import { AbillitiCognitoUserPool } from "./constructs/cognito-user-pool";
+import { Table } from "aws-cdk-lib/aws-dynamodb";
+import { InvoiceDDB } from "./constructs/invoice-ddb";
 
 export class AbillitiBackendStack extends Stack {
-  readonly userPool: UserPool;
+	readonly userPool: UserPool;
+	readonly invoiceTable: Table;
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
 
-    this.userPool = new AbillitiCognitoUserPool(
-      this,
-      "AbillitiCognitoUserPool"
-    ).userPool;
-  }
+		this.userPool = new AbillitiCognitoUserPool(
+			this,
+			"AbillitiCognitoUserPool"
+		).userPool;
+
+		this.invoiceTable = new InvoiceDDB(this, "AbillitiInvoiceTable").table;
+	}
 }
